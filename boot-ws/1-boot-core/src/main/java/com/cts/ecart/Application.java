1e9a3005ea2c94f2c00625245fe4cad5ba17754f
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2023-06-20 10:50:00 +0530

    spring boot

--- boot-ws/1-boot-core/src/main/java/com/cts/ecart/Application.java
+++ boot-ws/1-boot-core/src/main/java/com/cts/ecart/Application.java
@@ -2,9 +2,10 @@ package com.cts.ecart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication // auto discover
+public class Application { // container
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
