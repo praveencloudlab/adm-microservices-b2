aeb7206458e4084672858f1dd52339b4a5951ec2
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2023-07-06 10:48:50 +0530

    microservices

--- microservices-ws/temp/order-service/src/main/java/com/cts/ecart/dao/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package com.cts.ecart.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.ecart.entity.Order;
-
-public interface OrderDao extends JpaRepository<Order, Long> {
-
-}
--- microservices-ws/temp/price-service/src/main/java/com/cts/ecart/dao/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.cts.ecart.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.ecart.entity.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer>{
-	
-}
